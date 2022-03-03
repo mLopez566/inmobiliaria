d40797ae55fb48669302b14cff4fde36bc0353af
Marco López <marcolopez566@gmail.com>
2022-03-03 17:11:29 -0600

    mejoras a los carruceles

---
+++ index.js
@@ -0,0 +1,52 @@
+
+
+let h1 = document.getElementByTagName(h1);
+h1.addEventListener("mouseover", function(){
+    h1.style.color="red";
+})
+
+// // Get the modal
+// let modal = document.getElementById("myModal1");
+
+// // Get the image and insert it inside the modal - use its "alt" text as a caption
+// var img = document.getElementById("img1");
+// let modalImg = document.getElementById("img01");
+// let captionText = document.getElementById("caption");
+// img.onclick = function(){
+//   modal.style.display = "block";
+//   modalImg.src = this.src;
+//   captionText.innerHTML = this.alt;
+// }
+
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
+
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
